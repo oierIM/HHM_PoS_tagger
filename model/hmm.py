fd4f13e0647690a32367ce16b75057937a7b2b7c
ansuehu <andonisudupeehu@gmail.com>
2024-11-28 12:53:48 +0100

    Unos edits

--- model/hmm.py
+++ model/hmm.py
@@ -3,7 +3,8 @@ from collections import defaultdict, Counter
 
 class HMMPOSTagger:
     def __init__(self):
-        self.tags = set() #Set of tags
+        self.tags = set() #Set of tagsç
+        self.num_tags= len(self.tags)
         self.transition_probs = defaultdict(lambda:defaultdict(float)) #Dictionary to save transition probabilities
         self.emmision_probs = defaultdict(lambda:defaultdict(float)) #Dictionary to save emmision probabilities
         self.tag_counts = Counter() #To count each tag
@@ -13,7 +14,28 @@ class HMMPOSTagger:
         """
         Training the HMM given sentences and their pos_tags
         """
-        return 0
+
+        A = np.zeros(self.num_tags, self.num_tags)
+
+        # get the unique transition tuples (prev POS, cur POS)
+        trans_keys = set(transition_counts.keys())
+
+        for i in range(num_tags):
+            for j in range(num_tags):
+                # initialize the count of (prev POS, cur POS)
+                count = 0
+
+                key = (all_tags[i], all_tags[j])
+                if key in transition_counts:
+                    count = transition_counts[key]
+                count_prev_tag = tag_counts[all_tags[i]]
+
+                A[i, j] = (count) / (count_prev_tag)
+
+        return A
+        #for s in sentences:
+        #    for 
+        #return 0
     
     def calculate_probabilities(self, transition_counts, emmision_counts):
         """
