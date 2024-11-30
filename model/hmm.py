b1ab4a0f413c60d42e5ec7bca1ffca1027a4ece5
ansuehu <andonisudupeehu@gmail.com>
2024-11-30 18:40:23 +0100

    oier hiretzako 2

--- model/hmm.py
+++ model/hmm.py
@@ -5,8 +5,8 @@ class HMMPOSTagger:
     def __init__(self, tags):
         self.tags = tags
         self.num_tags= len(self.tags)
-        self.transition_counts = defaultdict(int)
-        self.emission_counts = defaultdict(int)
+        self.transition_counts = defaultdict(lambda:defaultdict(int))
+        self.emission_counts = defaultdict(lambda:defaultdict(int))
         self.transition_probs = defaultdict(lambda:defaultdict(float)) #Dictionary to save transition probabilities
         self.emmision_probs = defaultdict(lambda:defaultdict(float)) #Dictionary to save emmision probabilities
         self.word_counts = Counter() #To count each word
@@ -16,19 +16,24 @@ class HMMPOSTagger:
         """
         Training the HMM given sentences and their pos_tags
         """
+
+        self.get_counts(sentences, pos_tags, vocab)
         
     def get_counts(self, sentences, pos_tags, vocab):
 
         prev_tag = '*'
 
-        for word, tag in zip(sentences, pos_tags):
+        for sentence, tags in zip(sentences, pos_tags):
+            for word, tag in zip(sentence, tags):
 
-            if word not in vocab:
-                word = 'unk'
-            self.transition_counts[(prev_tag, tag)] += 1
-            self.emission_counts[(tag, word)] += 1
-            self.tag_counts[tag] += 1
-            prev_tag = tag
+                if word not in vocab:
+                    word = 'unk'
+                self.transition_counts[prev_tag][tag] += 1
+                self.emission_counts[tag][word] += 1
+                self.tag_counts[tag] += 1
+                self.word_counts[word] += 1
+                prev_tag = tag
+            self.transition_counts[prev_tag]["<STOP>"] +=1
 
     def create_transition_matrix(self):
         all_tags = sorted(self.tag_counts.keys())
