5ed86e602a1c67de1754e5dcc07560b84ea4293f
ansuehu <andonisudupeehu@gmail.com>
2024-12-05 15:35:42 +0100

    Animaciones locurones 2

--- main.py
+++ main.py
@@ -2,17 +2,6 @@ import numpy as np
 from model.hmm import HMMPOSTagger
 from collections import defaultdict, Counter
 from conllu_dataloader import *
-#Kaixo
-# def load_data():
-    
-#     training_corpus = "..."
-#     emission_counts, transition_counts, tag_counts = create_dictionaries(training_corpus, vocab2idx)
-#     states = sorted(tag_counts.keys())
-#     alpha = 0.001
-#     A = create_transition_matrix(transition_counts, tag_counts, alpha)
-#     B = create_emission_matrix(emission_counts, tag_counts, list(vocab2idx), alpha)
-#     save('A.npy', A)
-#     save('B.npy', B)
 
 def csv_to_list_of_lists(file_path):
     with open(file_path, 'r') as file:
